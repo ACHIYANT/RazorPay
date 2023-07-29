10653d9b954ebe97c7f085b3d4420bffceb4f684
ACHIYANT <achiyant@gmail.com>
2023-07-29 23:24:03 +0530

    Added custom breakpoints for media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,24 @@ module.exports = {
         'contain': 'contain',
         '100%': '100% 100%',
         '16': '4rem',
-      }
+        
+      },
+      screens: {
+        'max2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'maxxl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'maxlg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'maxmd': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'maxsm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      },
     },
   },
   plugins: [],
